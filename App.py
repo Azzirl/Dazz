8a14625c50ee3c8bc11d4eed562af80165602267
Azzir <carlossuqui53@gmail.com>
2026-09-04 10:31:50 -0500

    Update App.py

--- App.py
+++ App.py
@@ -1,4 +1,8 @@
-streamlit
-pandas
-plotly
-openpyxl
+import streamlit as st
+import pandas as pd
+import plotly.graph_objects as go
+
+# Configuración de la página
+st.set_page_config(page_title="EMS - Peak Shaving Fotovoltaico", layout="wide")
+
+st.title("Sistema de Gestión Inteligente de la Energía basado en Peak Shaving")
